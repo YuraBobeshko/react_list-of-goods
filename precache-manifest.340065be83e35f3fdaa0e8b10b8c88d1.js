ce8a483bcc40fd60e19bc41611e94d875d0d8d8e
YuraBobeshko <gorobech55@gmail.com>
2019-11-03 23:25:30 +0200

    Updates

---
+++ precache-manifest.340065be83e35f3fdaa0e8b10b8c88d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40af6426c89bf7d7c09255c25ba92ed2",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "aa19a42a60f1b42b0a5d",
+    "url": "/react_list-of-goods/static/js/2.26a51a8b.chunk.js"
+  },
+  {
+    "revision": "37fc1bb8a980a56ba074",
+    "url": "/react_list-of-goods/static/js/main.8565474c.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.340065be83e35f3fdaa0e8b10b8c88d1.js"
 );
 
 self.addEventListener('message', (event) => {
